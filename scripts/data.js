905d5a5c6576b37922c944c9a9d9e72af86b0ccd
Timothy Phillips <Tphill180@west-mec.local>
2021-04-09 16:36:32 -0700

    Modified data storage method.

---
+++ scripts/data.js
@@ -0,0 +1,215 @@
+const charmander = {
+	abilities: [
+		{
+			name: "blaze",
+		},
+		{
+			name: "solar-power",
+		},
+	],
+	base_experience: 62,
+	height: 6,
+	id: 4,
+	is_default: true,
+	name: "charmander",
+	order: 5,
+	stats: [
+		{
+			base_stat: 39,
+			effort: 0,
+			stat: {
+				name: "hp",
+			},
+		},
+		{
+			base_stat: 52,
+			effort: 0,
+			stat: {
+				name: "attack",
+			},
+		},
+		{
+			base_stat: 43,
+			effort: 0,
+			stat: {
+				name: "defense",
+			},
+		},
+		{
+			base_stat: 60,
+			effort: 0,
+			stat: {
+				name: "special-attack",
+			},
+		},
+		{
+			base_stat: 50,
+			effort: 0,
+			stat: {
+				name: "special-defense",
+			},
+		},
+		{
+			base_stat: 65,
+			effort: 1,
+			stat: {
+				name: "speed",
+			},
+		},
+	],
+	types: [
+		{
+			slot: 1,
+			type: {
+				name: "fire",
+			},
+		},
+	],
+	weight: 85,
+};
+
+const charizard = {
+  abilities: [
+    {
+      name: "blaze",
+    },
+    {
+      name: "solar-power",
+    },
+  ],
+  base_experience: 240,
+  height: 17,
+  id: 6,
+  is_default: true,
+  name: "charizard",
+  order: 7,
+  stats: [
+    {
+      base_stat: 78,
+      effort: 0,
+      stat: {
+        name: "hp",
+      },
+    },
+    {
+      base_stat: 84,
+      effort: 0,
+      stat: {
+        name: "attack",
+      },
+    },
+    {
+      base_stat: 78,
+      effort: 0,
+      stat: {
+        name: "defense",
+      },
+    },
+    {
+      base_stat: 109,
+      effort: 3,
+      stat: {
+        name: "special-attack",
+      },
+    },
+    {
+      base_stat: 85,
+      effort: 0,
+      stat: {
+        name: "special-defense",
+      },
+    },
+    {
+      base_stat: 100,
+      effort: 0,
+      stat: {
+        name: "speed",
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "fire",
+      },
+    },
+    {
+      slot: 2,
+      type: {
+        name: "flying",
+      },
+    },
+  ],
+  weight: 905,
+};
+
+const charmeleon = {
+  abilities: [
+    {
+      name: "blaze",
+    },
+    {
+      name: "solar-power",
+    },
+  ],
+  base_experience: 142,
+  height: 11,
+  id: 5,
+  is_default: true,
+  name: "charmeleon",
+  order: 6,
+  stats: [
+    {
+      base_stat: 58,
+      effort: 0,
+      stat: {
+        name: "hp",
+      },
+    },
+    {
+      base_stat: 64,
+      effort: 0,
+      stat: {
+        name: "attack",
+      },
+    },
+    {
+      base_stat: 58,
+      effort: 0,
+      stat: {
+        name: "defense",
+      },
+    },
+    {
+      base_stat: 80,
+      effort: 1,
+      stat: {
+        name: "special-attack",
+      },
+    },
+    {
+      base_stat: 65,
+      effort: 0,
+      stat: {
+        name: "special-defense",
+      },
+    },
+    {
+      base_stat: 80,
+      effort: 1,
+      stat: {
+        name: "speed",
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "fire",
+      },
+    },
+  ],
+  weight: 190,
+};
