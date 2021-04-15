a87247ea4d13de8b2cf2958d2a73239167c11eca
Timothy Phillips <Tphill180@west-mec.local>
2021-04-14 17:14:57 -0700

    Upgraded card animations.

--- scripts/script.js
+++ scripts/script.js
@@ -15,38 +15,40 @@ function loadPokemon(json, outputArea) {
 	// structure
 	outputArea.innerHTML += `
 		<div id="card-container" class="card-container">
-			<div class="card" id="card">
-				<div class="top" id="top">
-					<div class="top_data" id="top_data">
-						<div class="top_data_top" id="top_data_top">
-							<p id="name">${json.name}<p>
-							<div id="side_grouping">
-								<p id="hp"><span id="hp_disp">000</span> HP</p>
-								<img src="../assets/images/fire_type.svg" alt="type icon" id="type_icon">
+			<div class="card_holder" id="card_holder">	
+				<div class="card" id="card">
+						<div class="top" id="top">
+							<div class="top_data" id="top_data">
+								<div class="top_data_top" id="top_data_top">
+									<p id="name">${json.name}<p>
+									<div id="side_grouping">
+										<p id="hp"><span id="hp_disp">000</span> HP</p>
+										<img src="../assets/images/fire_type.svg" alt="type icon" id="type_icon">
+									</div>
+								</div>
+								<div class="top_data_bottom" id="top_data_bottom">
+									<img src="../assets/images/${json.name}.jpg" alt="image of pokemon" id="image_of">
+								</div>
 							</div>
 						</div>
-						<div class="top_data_bottom" id="top_data_bottom">
-							<img src="../assets/images/${json.name}.jpg" alt="image of pokemon" id="image_of">
+						<div class="middle" id="middle">
+							<div class="middle_base_info" id="middle_base_info">
+								<p><span id="type_value">TYPE</span> Pokémon.</p>
+								<p>Height: <span id="height_value">HEIGHT</span>',</p>
+								<p>Weight: <span id="weight_value">WEIGHT</span> lbs.</p>
+							</div>
+							<div class="middle_abilities" id="middle_abilities">
+
+							</div>
+
+							<div class="middle_stats" id="middle_stats">
+
+							</div>
+						</div>
+						<div class="bottom" id="bottom">
+							<p id="license_info">All Images/Names © The Pokemon Company
 						</div>
 					</div>
-				</div>
-				<div class="middle" id="middle">
-					<div class="middle_base_info" id="middle_base_info">
-						<p><span id="type_value">TYPE</span> Pokémon.</p>
-						<p>Height: <span id="height_value">HEIGHT</span>',</p>
-						<p>Weight: <span id="weight_value">WEIGHT</span> lbs.</p>
-					</div>
-					<div class="middle_abilities" id="middle_abilities">
-
-					</div>
-
-					<div class="middle_stats" id="middle_stats">
-
-					</div>
-				</div>
-				<div class="bottom" id="bottom">
-					<p id="license_info">All Images/Names © The Pokemon Company
-				</div>
 			</div>
 		</div>
 	`;
