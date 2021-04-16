7d526642b021a302b7dfca67796d31cf812c6cd0
David <david.nevarez03@hotmail.com>
2021-04-15 18:26:43 -0700

    upadated homepage

--- scripts/ballAnimation.js
+++ scripts/ballAnimation.js
@@ -1,7 +1,38 @@
-let ball = document.getElementsByName("pokeball")
+// let ball = document.getElementsById("pokeball");
 
-ball.addEventListener("mouseover", function() {
-    // What ever you want to happen when the mose is over the pokeball
 
-    let timer = setInterval()
-})
+// ball.addEventListener("mouseover", function() {
+//     anime.remove('#pokeball');
+//     let animation = anime({
+//         targets: '#pokeball',
+//         rotate: [30, -30],
+//         easing: 'easeInOutSine',
+//         duration: 2000,
+//         direction: 'noraml'
+//     })
+// })
+
+
+// let animation = anime.timeline({
+//     duration: 2000,
+//     easing: 'easeInOutSine',
+//     direction: 'alternate',
+//     loop: true
+// });
+
+// animation.add({
+//     targets: '.pokeball',
+//     keyframes: [
+//         { translateY: 0 },
+//         { translateX: -80 },
+//         { translateY: -160 },
+//     ],
+//     duration: 4000,
+//     delay: anime.stagger(100)
+//     easing: 'easeOutElastic(1, .8)',
+//     loop: true
+// }).add({
+//     targets: '',
+//     translateY: -50,
+//     backgroundColor: 'rgb(0, 255, 0)'
+// })
